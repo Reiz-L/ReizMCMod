dfc889b931d875720776424ee48b86dc2e377992
Reiz-L <kokodayoo@126.com>
2020-04-06 08:42:18 +0800

    最后一次更新

--- src/main/java/com/reiz/funmod/energy/ElecticityEnergyStorage.java
+++ src/main/java/com/reiz/funmod/energy/ElecticityEnergyStorage.java
@@ -1,5 +1,14 @@
 package com.reiz.funmod.energy;
 
-public class ElecticityEnergyStorage {
+import net.minecraftforge.energy.EnergyStorage;
+
+public class ElecticityEnergyStorage extends EnergyStorage{
+
+	public ElecticityEnergyStorage(int capacity, int maxReceive, int maxExtract) {
+		super(capacity, maxReceive, maxExtract);
+		// TODO 自动生成的构造函数存根
+	}
+
+
 
 }
--- src/main/java/com/reiz/funmod/main.java
+++ src/main/java/com/reiz/funmod/main.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class main {
 	public static final String MODID = "reizmod";
     public static final String NAME = "Reiz's Mod";
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.0.7";
 	@Instance
 	public static main instance = new main();
 	
